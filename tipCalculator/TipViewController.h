f1045c4a11f6d01a4151592f09d501e8642883ff
liuqiyuan <i@liuqiyuan.com>
2015-01-19 20:26:18 -0800

    tip calculator project for iOS class

---
+++ tipCalculator/TipViewController.h
@@ -0,0 +1,13 @@
+//
+//  TipViewController.h
+//  tipCalculator
+//
+//  Created by Qiyuan Liu on 1/19/15.
+//  Copyright (c) 2015 Yahoo!. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface TipViewController : UIViewController
+
+@end
---
+++ tipCalculator/TipViewController.m
@@ -0,0 +1,67 @@
+//
+//  TipViewController.m
+//  tipCalculator
+//
+//  Created by Qiyuan Liu on 1/19/15.
+//  Copyright (c) 2015 Yahoo!. All rights reserved.
+//
+
+#import "TipViewController.h"
+
+@interface TipViewController ()
+@property (weak, nonatomic) IBOutlet UITextField *billTextField;
+@property (weak, nonatomic) IBOutlet UILabel *tipLable;
+@property (weak, nonatomic) IBOutlet UILabel *totalLable;
+@property (weak, nonatomic) IBOutlet UISegmentedControl *tipControl;
+- (IBAction)onTap:(id)sender;
+- (void)updateValues;
+
+@end
+
+@implementation TipViewController
+
+- (id)initWithNibName:(NSString *)nibNameOrNil bundle:(NSBundle *)nibBundleOrNil
+{
+    self = [super initWithNibName:@"TipViewController" bundle:nil];
+    if (self) {
+        self.title = @"Tip Calculator";
+    }
+    return self;
+}
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view from its nib.
+    [self updateValues];
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+/*
+#pragma mark - Navigation
+
+// In a storyboard-based application, you will often want to do a little preparation before navigation
+- (void)prepareForSegue:(UIStoryboardSegue *)segue sender:(id)sender {
+    // Get the new view controller using [segue destinationViewController].
+    // Pass the selected object to the new view controller.
+}
+*/
+
+- (IBAction)onTap:(id)sender {
+    [self.view endEditing:YES];
+    [self updateValues];
+}
+
+- (void)updateValues {
+    float billAmount = [self.billTextField.text floatValue];
+    NSArray *tipValues = @[@(0.1), @(0.15), @(0.2), @(0.25), @(0.3)];
+    float tipAmount = billAmount * [tipValues[self.tipControl.selectedSegmentIndex] floatValue];
+    float totalAmount = tipAmount + billAmount;
+    self.tipLable.text = [NSString stringWithFormat:@"$%0.2f", tipAmount];
+    self.totalLable.text = [NSString stringWithFormat:@"$%0.2f", totalAmount];
+    
+}
+@end
